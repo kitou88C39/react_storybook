84f4d3e455883ad6aee9612ddd3e968c21867ebd
kitou88C39 <kanfutrooper@icloud.com>
2023-07-09 11:40:53 +0900

    Button.stories.jsxを編集

--- src/components/Button/Button.stories.jsx
+++ src/components/Button/Button.stories.jsx
@@ -1,9 +1,9 @@
-import Button from './Button';
+import Button from '../Button/Button';
 
 export default {
   title: 'Common/Button',
   component: Button,
 };
 
-export const HelloButton = () => <Button>Hello World!</Button>;
-export const ClickButton = () => <Button>Click!</Button>;
+export const Default = () => <Button>Default</Button>;
+export const Primary = () => <Button color='primary'>Primary</Button>;
