5d5bb4d5ea36bb58be83d0985048f4e98fe46bab
kitou88C39 <kanfutrooper@icloud.com>
2023-07-09 14:45:47 +0900

    Button.stories.jsxを編集

--- src/components/Button/Button.stories.jsx
+++ src/components/Button/Button.stories.jsx
@@ -6,4 +6,15 @@ export default {
 };
 
 export const Default = () => <Button>Default</Button>;
+export const Danger = () => <Button color='danger'>Danger</Button>;
 export const Primary = () => <Button color='primary'>Primary</Button>;
+export const PrimarySmall = () => (
+  <Button size='sm' color='primary'>
+    Small
+  </Button>
+);
+export const PrimaryLarge = () => (
+  <Button size='lg' color='primary'>
+    Large
+  </Button>
+);
